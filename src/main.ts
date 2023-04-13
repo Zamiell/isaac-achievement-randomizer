3c9c1d500441ebd92675b77ac4101d6827b40cbc
James <5511220+Zamiell@users.noreply.github.com>
2023-04-13 19:11:50 -0400

    chore: add files from IsaacScript 3.6.45 template

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { ModCallback } from "isaac-typescript-definitions";
+
+const MOD_NAME = "isaac-achievement-randomizer";
+
+main();
+
+function main() {
+  // Instantiate a new mod object, which grants the ability to add callback functions that
+  // correspond to in-game events.
+  const mod = RegisterMod(MOD_NAME, 1);
+
+  // Register a callback function that corresponds to when a new player is initialized.
+  mod.AddCallback(ModCallback.POST_PLAYER_INIT, postPlayerInit);
+
+  // Print a message to the "log.txt" file.
+  Isaac.DebugString(`${MOD_NAME} initialized.`);
+}
+
+function postPlayerInit() {
+  Isaac.DebugString("Callback fired: POST_PLAYER_INIT");
+}
